e375911a8f34e9464c2c7fd8462f2a543b51b7a7
akanistudio <akanistudio@gmail.com>
2021-04-25 05:37:42 +0200

    approach changed, removing executable from base image entrypoint and calling it on my own

--- akani-entrypoint.sh
+++ akani-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-(sleep 15 
+sed -i '/exec "$@"/d' /usr/local/bin/docker-entrypoint.sh 
+/usr/local/bin/docker-entrypoint.sh "$@"
 cd /var/www/html/
 rm -rf /var/www/html/wp-content/plugins/akismet
-rm -f /var/www/html/wp-content/plugins/hello.php) &
-/usr/local/bin/docker-entrypoint.sh "$@"
+rm -f /var/www/html/wp-content/plugins/hello.php
+exec "$@"
