1cdc585e5a78c51d94e9a5dbbfb3e429b2624294
akanistudio <akanistudio@gmail.com>
2021-09-25 22:21:36 +0200

    composer prepare config file for wordpress

--- akani-composer.sh
+++ akani-composer.sh
@@ -7,3 +7,17 @@ rm -R -- */
 # copy whole wordpress
 cd ../..
 cp -rf * ../../..
+
+# prepare config file
+cd ../../..
+cp wp-config-sample.php wp-config.php
+sed -i -e "s/database_name_here/$1/g" wp-config.php
+sed -i -e "s/username_here/$2/g" wp-config.php
+sed -i -e "s/password_here/$3/g" wp-config.php
+sed -i -e "s/localhost/$4/g" wp-config.php
+
+count=8
+for i in $(seq $count); do
+    SALT=`openssl rand -hex 64`
+	sed -i -e "/put your unique phrase here/{s//${SALT}/;:a" -e '$!N;$!ba' -e '}' wp-config.php
+done
